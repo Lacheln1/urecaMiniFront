caa0f80cf95afbf9efd08f765448e9e586eca534
Lacheln <psionict@naver.com>
2025-03-18 14:30:32 +0900

    새글작성 버튼 css 추가

--- js/member.js
+++ js/member.js
@@ -121,7 +121,7 @@ function updateUIAfterLogin(username) {
         </ul>
     `;
     authContainer.appendChild(userDropdown);
-    newPostBtnLayout.innerHTML=`<button id ="newPostBtn">새 글 작성</button>`;
+    newPostBtnLayout.innerHTML=`<button id ="newPostBtn" class = "btn" style="border:1px solid #000">새 글 작성</button>`;
     document.getElementById("newPostBtn").addEventListener("click", () => {
         window.location.href = "../writePost.html";
     });
