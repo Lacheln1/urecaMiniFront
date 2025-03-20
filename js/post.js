76a4b42741bdd2f2f72cf89b2120f16641f59176
Lacheln <psionict@naver.com>
2025-03-20 15:18:34 +0900

    필요 없는 코드 제거
    주석 처리한 코드 정리

--- js/post.js
+++ js/post.js
@@ -10,7 +10,7 @@ document.getElementById("postBtn").addEventListener("click",async()=>{
     try{
         const response = await axios.post("http://localhost:8080/insertPost",data);
         console.log(response);
-        alert("완료");
+        alert("게시 완료");
         window.location.href = "../index.html";
     } catch(error){
         alert("발간 실패하였습니다",error);
@@ -23,67 +23,3 @@ document.getElementById("cancelBtn").addEventListener("click",async ()=>{
         window.location.href = "/";
     }
 })
-
-
-
-// function updatePostImage(postImageUrl){
-//     const postImage = document.getElementsByClassName("post-thumbnail");
-
-//     if(!postImageUrl || postImageUrl.trim()===""){
-//         console.log("이미지 없음");
-//         postImage.src="http://localhost:8080/thumbnail/no-intro.png"; // 기본 이미지
-//         return;
-//     }
-
-//     let imageUrl = postImageUrl.startsWith("/thumbnail/")
-//     ? `http://localhost:8080${profileImageUrl}`
-//     : `http://localhost:8080/thumbnail/${profileImageUrl}`;
-
-//     // 캐시 문제 방지 (새로운 이미지 강제 로드)
-//     const timestamp = new Date().getTime();
-//     profileImage.src = `${imageUrl}?t=${timestamp}`;
-
-//     console.log("프로필 이미지 업데이트됨:", profileImage.src);
-// }
-    
-// document.getElementById("imgBtn").addEventListener("click", function () {
-//     const fileInput = document.createElement("input");
-//     fileInput.type = "file";
-//     fileInput.accept = "image/*";
-
-//     fileInput.addEventListener("change", async function () {
-//         const file = fileInput.files[0];
-//         if (!file) return;
-
-//         const formData = new FormData();
-//         formData.append("file", file);
-
-//         const token = localStorage.getItem("jwtToken")?.replace(/\"/g, "").trim();
-
-//         if (!token) {
-//             alert("로그인이 필요합니다.");
-//             return;
-//         }
-
-//         try {
-//             const response = await fetch("http://localhost:8080/upload-post-image", {  
-//                 method: "POST",
-//                 headers: { "Authorization": `Bearer ${token}` },
-//                 body: formData
-//             });
-
-//             if (!response.ok) throw new Error("이미지 업로드 실패");
-
-//             const { postImageUrl } = await response.json();
-//             updateProfileImage(postImageUrl);  //수정된 함수 호출
-
-//             alert("이미지 업로드 완료!");
-
-//         } catch (error) {
-//             console.error("이미지 업로드 오류:", error);
-//             alert("이미지 업로드 실패!");
-//         }
-//     });
-
-//     fileInput.click();
-// });
